bad79aec714d66e0b2e26b56732213f972b86031
AsliddinMamadiyev <asliddinmamadiyev006@gmail.com>
2024-10-28 16:29:50 +0500

    strings

---
+++ string-metod/app.js
@@ -0,0 +1,54 @@
+"use strict";
+
+// function str(word) {
+//     return word.slice(0, word.length -1);
+// }
+
+// console.log(str(javascript));
+
+// function containsJavaScript(str) {
+//     return str.indexOf('JavaScript') !== -1;
+// }
+
+// console.log(containsJavaScript("I love JavaScript"));
+// console.log("I love coding");
+
+// function extraWorld(str, word) {
+//     let res = str.indexOf(word);
+//     if(res === -1) {
+//         return str
+//     }
+// }
+// console.log(extraWorld("hello world"));
+
+// ----1----
+// function capitalize(word) {
+//   if (!word) return word; 
+//   return word[0].toUpperCase() + word.slice(1).toLowerCase();
+// }
+// console.log(capitalize("javascript"));
+// console.log(capitalize("HELLO"));
+
+// ----2----
+// function capitalize(word) {
+//     let newWord = word.slice(0, - 1);
+//     return newWord;
+// }
+// console.log(capitalize("javascript"));
+// console.log(capitalize("HELLO"));
+
+// ----4----
+// function extractWorld(str, kesiladiganSoz) {
+
+// }
+//     console.log(extractWorld("Salom dunyo", "Salom"));
+
+// ----5----
+// function trimString(str) {
+//     let newStr = str.trim();
+//     return newStr;
+//     }
+//     console.log(trimString(" hello world "));
+//     console.log(trimString(" JavaScript "));
+
+// ----6----
